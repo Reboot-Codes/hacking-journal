e55a6211c204cee97206e0b1bdba572a2d0598c9
Reboot-Codes <55662140+Reboot-Codes@users.noreply.github.com>
2021-10-18 15:10:15 -0700

    Fixed Import Issue in index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -3,8 +3,8 @@ import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './utils/reportWebVitals';
+import * as serviceWorker from './utils/serviceWorker';
 
 ReactDOM.render(
   <StrictMode>
